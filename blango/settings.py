9c46375cb0b7f0a71a6bd84edcfcef7e4a45932e
Maria Morillo <mricomorillo@gmail.com>
2022-06-14 12:39:16 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -65,6 +65,9 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+    ], 
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ]
   }
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
