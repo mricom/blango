552dea05877c6b481e3d88c4ee58e557e84a5672
Maria Morillo <mricomorillo@gmail.com>
2022-08-29 11:09:25 +0000

    Finish filtering

--- blog/api/views.py
+++ blog/api/views.py
@@ -13,6 +13,12 @@ from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSeria
 from blog.models import Post, Tag
 from blango_auth.models import User
 
+from django.db.models import Q
+from django.utils import timezone
+
+from datetime import timedelta
+from django.http import Http404
+
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
@@ -34,8 +40,44 @@ class PostViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     @method_decorator(cache_page(120))
+    @method_decorator(vary_on_headers("Authorization", "Cookie"))
     def list(self, *args, **kwargs):
         return super(PostViewSet, self).list(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            # published only
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
+
+        elif not self.request.user.is_staff:
+            # allow all
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(
+                Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+            )
+
+        time_period_name = self.kwargs.get("period_name")
+
+        if not time_period_name:
+            # no further filtering required
+            return queryset
+
+        if time_period_name == "new":
+            return queryset.filter(
+                published_at__gte=timezone.now() - timedelta(hours=1)
+            )
+        elif time_period_name == "today":
+            return queryset.filter(
+                published_at__date=timezone.now().date(),
+            )
+        elif time_period_name == "week":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+        else:
+            raise Http404(
+                f"Time period {time_period_name} is not valid, should be "
+                f"'new', 'today' or 'week'"
+            )
 
 
 
