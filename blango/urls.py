e81606dfd7351ea9a85ab663cbcb632e8badedf7
Maria Morillo <mricomorillo@gmail.com>
2022-08-31 08:29:28 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="post-detail"),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
     path("ip/", blog.views.get_ip),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path(
---
+++ blog/static/blog/blog.js
@@ -0,0 +1,8 @@
+const theNumber = 1
+
+if (theNumber === 2) {
+  let yourName = 'Leo'
+  alert(yourName)
+}
+
+alert(yourName)
